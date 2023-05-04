7f29b80699deeecf7d48fb464855985397fe9521
bretsky <bretskyselby99@gmail.com>
2023-05-03 21:55:24 -0700

    Revert "Analytics"
    This reverts commit 763fd4fdf607557a6653ec6bed995d419ca8f349.

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,6 @@
 	import './styles.css';
 	import {userid, token} from '../stores.ts';
 	import { goto } from '$app/navigation';
-	import { dev } from '$app/environment';
-	import { inject } from '@vercel/analytics';
-	
-	inject({ mode: dev ? 'development' : 'production' });
 
 	const logout = () => {
 		$userid = null;
